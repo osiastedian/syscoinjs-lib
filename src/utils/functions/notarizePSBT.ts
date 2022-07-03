abd8405957f663ae33856cb7bc4b92a4582f7dcb
gabriel <0xgabrielp@gmail.com>
2022-07-03 09:41:49 +0800

    feat(): notarizePSBT() - TS version

---
+++ src/utils/functions/notarizePSBT.ts
@@ -0,0 +1,21 @@
+import { Psbt } from "bitcoinjs-lib";
+import { NotaryAsset } from "../../types/notary-details";
+import getNotarizationSignatures from "./getNotarizationSignatures";
+import syscointx from 'syscointx-js';
+
+/**
+ * Purpose: Notarize Result object from syscointx.createTransaction()/syscointx.createAssetTransaction() if required by the assets in the inputs of the transaction
+ * @param psbt Required. The resulting PSBT object passed in which is assigned from syscointx.createTransaction()/syscointx.createAssetTransaction()
+ * @param notaryAssets Required. Asset objects require notarization, fetch signatures via fetchNotarizationFromEndPoint()
+ * @param rawTx Required. Transaction hex
+ * @returns 
+ */
+export async function notarizePSBT (psbt: Psbt, notaryAssets: Map<string, NotaryAsset>, rawTx: string): Promise<boolean> {
+    const notarizationDone = await getNotarizationSignatures(notaryAssets, rawTx)
+    if (notarizationDone) {
+      return syscointx.addNotarizationSignatures(psbt.version, notaryAssets, psbt.txOutputs)
+    }
+    return false
+  }
+
+  export default notarizePSBT;
