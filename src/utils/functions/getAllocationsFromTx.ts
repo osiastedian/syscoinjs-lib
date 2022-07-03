7ba7763d77a83904d0b8dc009a3f3f0511dee03a
gabriel <0xgabrielp@gmail.com>
2022-07-03 09:19:44 +0800

    feat(): getAllocationsFromTx() - TS version

---
+++ src/utils/functions/getAllocationsFromTx.ts
@@ -0,0 +1,15 @@
+import syscointx from 'syscointx-js';
+import bjs from 'bitcoinjs-lib'
+import { AssetAllocation } from '../../types/asset-allocation';
+
+/**
+ * Purpose: Return allocation information for an asset transaction. Pass through to syscointx-js
+ * @param tx Required. bitcoinjs transaction
+ * @returns allocation information
+ */
+export function getAllocationsFromTx (tx: bjs.Transaction): AssetAllocation {
+    return syscointx.getAllocationsFromTx(tx) || []
+  }
+  
+
+export default getAllocationsFromTx
