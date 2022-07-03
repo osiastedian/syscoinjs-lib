61a2164f3cbb8a9abc659dfd1aa0d5db82cb275e
gabriel <0xgabrielp@gmail.com>
2022-07-03 09:19:44 +0800

    feat(): getAllocationsFromTx() - TS version

---
+++ src/types/asset-allocation.ts
@@ -0,0 +1,10 @@
+import BN from "bn.js";
+
+export interface AssetAllocation  {
+    assetGuid: string;
+    values: {
+        n: number;
+        value: BN;
+    }[];
+    notarysig: string | Buffer;
+}[]
